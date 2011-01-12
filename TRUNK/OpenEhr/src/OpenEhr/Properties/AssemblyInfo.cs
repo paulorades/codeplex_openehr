8d432b48770b1037aae73fe942eb69c82c2a7002
heathfrankel_cp <SND\heathfrankel_cp>
2011-01-12 14:40:41 -0800

    Replaced OceanEhr.snk with new OpenEhrNet.snk Updated AssemblyInfo

--- TRUNK/OpenEhr/src/OpenEhr/Properties/AssemblyInfo.cs
+++ TRUNK/OpenEhr/src/OpenEhr/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("OpenEhrV1")]
-[assembly: AssemblyDescription("openEHR Reference Model V1.0.1")]
+[assembly: AssemblyTitle("OpenEHR")]
+[assembly: AssemblyDescription("openEHR V1.0.1")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Ocean Informatics")]
-[assembly: AssemblyProduct("OceanEhr")]
-[assembly: AssemblyCopyright("Copyright © Ocean Informatics 2006")]
+[assembly: AssemblyCompany("Ocean Informatics Pty Ltd")]
+[assembly: AssemblyProduct("OpenEHR.NET")]
+[assembly: AssemblyCopyright("Copyright © Ocean Informatics Pty Ltd 2011")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.5.3209.0")]
+[assembly: AssemblyVersion("1.0.*")]
 //[assembly: AssemblyFileVersion("1.0.0.0")]
